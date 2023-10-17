e10b0b6625544913a490dc70332b25c7625d8a4b
Aste21 <131811810+Aste21@users.noreply.github.com>
2023-10-17 22:56:32 +0200

    Update TestComplexNumbers.cpp

--- TestComplexNumbers.cpp
+++ TestComplexNumbers.cpp
@@ -13,6 +13,7 @@ int main() {
 
     // Test addition and subtraction
 
+    std::cout << std::endl << std::endl << std::endl;
     std::cout << "a + b - c: " << a + b - c << " Correct: 0 + 2i" << std::endl;
     std::cout << "((a += b) += c): " << ((a += b) += c) << " Correct: 6 + 6i" << std::endl;
     std::cout << "a: " << a << " Correct: 6 + 6i" << std::endl;
@@ -23,19 +24,9 @@ int main() {
     b = Complex(1.0, 1.0);
     c = Complex(3.0, 2.0);
 
-    // Test multiplication
-
-    std::cout << "a * b * c: " << a * b * c << " Correct: -13 + 13i" << std::endl;
-    std::cout << "((a *= b) *= c): " << ((a *= b) *= c) << " Correct: -13 + 13i" << std::endl;
-    std::cout << "a: " << a << " Correct: -13 + 13i" << std::endl;
-    std::cout << "b: " << b << " Correct: 1 + 1i" << std::endl;
-    std::cout << "c: " << c << " Correct: 3 + 2i" << std::endl;
-    a = Complex(2.0, 3.0);
-    b = Complex(1.0, 1.0);
-    c = Complex(3.0, 2.0);
-
     // Test a += b += c
 
+    std::cout << std::endl << std::endl << std::endl;
     std::cout << "(a += b += c): " << (a += b += c) << " Correct: 6 + 6i" << std::endl;
     std::cout << "a: " << a << " Correct: 6 + 6i" << std::endl;
     std::cout << "b: " << b << " Correct: 4 + 3i" << std::endl;
@@ -45,8 +36,21 @@ int main() {
     b = Complex(1.0, 1.0);
     c = Complex(3.0, 2.0);
 
-    // Test a *= b *= c
+    // Test multiplication
 
+    std::cout << std::endl << std::endl << std::endl;
+    std::cout << "a * b * c: " << a * b * c << " Correct: -13 + 13i" << std::endl;
+    std::cout << "((a *= b) *= c): " << ((a *= b) *= c) << " Correct: -13 + 13i" << std::endl;
+    std::cout << "a: " << a << " Correct: -13 + 13i" << std::endl;
+    std::cout << "b: " << b << " Correct: 1 + 1i" << std::endl;
+    std::cout << "c: " << c << " Correct: 3 + 2i" << std::endl;
+    a = Complex(2.0, 3.0);
+    b = Complex(1.0, 1.0);
+    c = Complex(3.0, 2.0);
+
+
+    // Test a *= b *= c
+    std::cout << std::endl << std::endl << std::endl;
     std::cout << "(a *= b *= c): " << (a *= b *= c) << " Correct: -13 + 13i" << std::endl;
     std::cout << "a: " << a << " Correct: -13 + 13i" << std::endl;
     std::cout << "b: " << b << " Correct: 1 + 5i" << std::endl;
@@ -58,6 +62,7 @@ int main() {
 
     // Test addition and multiplication with real numbers
 
+    std::cout << std::endl << std::endl << std::endl;
     std::cout << "a + 7: " << a + 7.0 << " Correct: 9 + 3i" << std::endl;
     std::cout << "7 + a: " << 7.0 + a << " Correct: 9 + 3i" << std::endl;
     std::cout << "b * 5: " << b * 5.0 << " Correct: 5 + 5i" << std::endl;
@@ -65,6 +70,7 @@ int main() {
 
     // Test equality comparisons
 
+    std::cout << std::endl << std::endl << std::endl;
     std::cout << "c == 3.5: " << (c == 3.5 ? "true" : "false") << " Correct: false" << std::endl;
     std::cout << "3.5 == c: " << (3.5 == c ? "true" : "false") << " Correct: false" << std::endl;
 
@@ -75,14 +81,16 @@ int main() {
     Complex g(7.0); // Construct with a real part, imaginary part defaults to 0
     Complex h;      // Default constructor, both real and imaginary parts are 0
 
+    std::cout << std::endl << std::endl << std::endl;
     std::cout << "d: " << d << " Correct: 4 + 5i" << std::endl;
     std::cout << "e: " << e << " Correct: 2 + 2i" << std::endl;
     std::cout << "f: " << f << " Correct: 1 + 3i" << std::endl;
-    std::cout << "g: " << g << " Correct: 7" << std::endl;
-    std::cout << "h: " << h << " Correct: 0" << std::endl;
+    std::cout << "g: " << g << " Correct: 7 + 0i" << std::endl;
+    std::cout << "h: " << h << " Correct: 0 + 0i" << std::endl;
 
     // Test addition, subtraction
 
+    std::cout << std::endl << std::endl << std::endl;
     std::cout << "d + e: " << d + e << " Correct: 6 + 7i" << std::endl;
     std::cout << "d - e: " << d - e << " Correct: 2 + 3i" << std::endl;
     std::cout << "(d += e): " << (d += e) << " Correct: 6 + 7i" << std::endl;
@@ -90,29 +98,31 @@ int main() {
     std::cout << "e: " << e << " Correct: 2 + 2i" << std::endl;
     d = Complex(4.0, 5.0);
     e = Complex(2.0, 2.0);
-    std::cout << "(d -= e): " << (d -= e) << " Correct: 4 + 5i" << std::endl;
-    std::cout << "d: " << d << " Correct: 4 + 5i" << std::endl;
+    std::cout << "(d -= e): " << (d -= e) << " Correct: 2 + 3i" << std::endl;
+    std::cout << "d: " << d << " Correct: 2 + 3i" << std::endl;
     std::cout << "e: " << e << " Correct: 2 + 2i" << std::endl;
     d = Complex(4.0, 5.0);
     e = Complex(2.0, 2.0);
 
     // Test multiplication, division
 
+    std::cout << std::endl << std::endl << std::endl;
     std::cout << "d * e: " << d * e << " Correct: -2 + 18i" << std::endl;
     std::cout << "d / e: " << d / e << " Correct: 2.25 + 0.25i" << std::endl;
-    std::cout << "(d *= e): " << (d *= e) << " Correct: 6 + 18i" << std::endl;
-    std::cout << "d: " << d << " Correct: 6 + 18i" << std::endl;
+    std::cout << "(d *= e): " << (d *= e) << " Correct: -2 + 18i" << std::endl;
+    std::cout << "d: " << d << " Correct: -2 + 18i" << std::endl;
     std::cout << "e: " << e << " Correct: 2 + 2i" << std::endl;
     d = Complex(4.0, 5.0);
     e = Complex(2.0, 2.0);
-    std::cout << "(d /= e): " << (d /= e) << " Correct: 2.5 + 0.5i" << std::endl;
-    std::cout << "d: " << d << " Correct: 2.5 + 0.5i" << std::endl;
+    std::cout << "(d /= e): " << (d /= e) << " Correct: 2.25 + 0.25i" << std::endl;
+    std::cout << "d: " << d << " Correct: 2.25 + 0.25i" << std::endl;
     std::cout << "e: " << e << " Correct: 2 + 2i" << std::endl;
     d = Complex(4.0, 5.0);
     e = Complex(2.0, 2.0);
 
     // Test equality comparisons
 
+    std::cout << std::endl << std::endl << std::endl;
     std::cout << "d == d: " << (d == d ? "true" : "false") << " Correct: true" << std::endl;
     std::cout << "d == e: " << (d == e ? "true" : "false") << " Correct: false" << std::endl;
     std::cout << "d != d: " << (d != d ? "true" : "false") << " Correct: false" << std::endl;
@@ -122,6 +132,7 @@ int main() {
     float phaseD = d.returnPhase();
     float amplitudeD = d.returnAmplitude();
 
+    std::cout << std::endl << std::endl << std::endl;
     std::cout << "Phase of d: " << phaseD << " radians" << " Correct: 0.896055" << std::endl;
     std::cout << "Amplitude of d: " << amplitudeD << " Correct: 6.40312" << std::endl;
 
