6c0d49854c3931c3eea866b60d4718b472429271
Wiktor Kopczyński <131811810+Aste21@users.noreply.github.com>
2023-11-09 09:52:19 +0100

    Add files via upload

---
+++ TestComplexNumbers.h
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+#pragma once
+
+void testClassicConstructor();
+
+void testAdditionAndSubstraction();
+
+void testOfAdditionAssignment();
+
+void testOfMultiplication();
+
+void testOfMultiplicationAssignment();
+
+void testOfAdditionAndSubstractionWithRealNumbers();
+
+void testOfEqualityComparionBetweenComplexAndReal();
+
+void testConstructorOverload();
+
+void anotherTestOfAdditionAndSubstraction();
+
+void testMultiplicationAndDivision();
+
+void testEqualityComparisons();
+
+void testOfPhaseAndAmplitudeFunctions();
---
+++ TestComplexNumbersMain.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "ComplexNumbers.h"
+#include "TestComplexNumbers.h"
+
+
+int main()
+{
+    testClassicConstructor();
+
+    testAdditionAndSubstraction();
+
+    testOfAdditionAssignment();
+
+    testOfMultiplication();
+
+    testOfMultiplicationAssignment();
+
+    testOfAdditionAndSubstractionWithRealNumbers();
+
+    testOfEqualityComparionBetweenComplexAndReal();
+
+    testConstructorOverload();
+
+    anotherTestOfAdditionAndSubstraction();
+
+    testMultiplicationAndDivision();
+
+    testEqualityComparisons();
+
+    testOfPhaseAndAmplitudeFunctions();
+
+    return 0;
+}
